33f1ca345e6edc7fd9e5cd44efaf6a1ab54a9701
Santiago <pastorsantiago99@gmail.com>
2025-09-26 05:25:56 -0300

    Nuevas funciones

---
+++ src/funciones.py
@@ -0,0 +1,16 @@
+def calcular_puntaje (item):
+    equipo,datos=item
+    puntos= 3 * datos['innovacion'] + datos['presentacion']
+    if datos['errores']:
+        puntos -= 1
+    return equipo, puntos
+
+def mostrar_tabla (resultados):
+    tabla = sorted(resultados.items(), key=lambda x: x[1]['puntos'], reverse=True)
+    print(f"{'Equipo':<10}{'Puntos':<8}{'Innov.':<8}{'Pres.':<8}{'Errores':<10}{'MER':<5}")
+    print("-" *50)
+    for equipo, stats in tabla:
+        print(f"{equipo:<10}{stats['puntos']:<8}{stats['innovacion']:<8}{stats['presentacion']:<8}{stats['errores']:<10}{stats['mer']:<5}")
+        
+def equipos_con_errores (ronda):
+    return list(map(lambda x: x[0], filter(lambda x: x[1]['errores'], ronda.items())))
